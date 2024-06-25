65e14150db70692de499c31f8eb7fbf671c891ee
Sviut <jeleznov88@gmail.com>
2024-06-25 13:43:45 +0300

    add product module

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { ProductService } from './product.service'
+import { ProductDto } from './dto/product.dto'
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll(@Query('searchTerm') searchTerm?: string) {
+		return this.productService.getAll(searchTerm)
+	}
+
+	@Get('by-slug/:slug')
+	async getBySlug(@Param('slug') slug: string) {
+		return this.productService.getBySlug(slug)
+	}
+
+	@Get('by-category/:categorySlug')
+	async getByCategory(@Param('categorySlug') categorySlug: string) {
+		return this.productService.getByCategory(categorySlug)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(HttpStatus.OK)
+	@Post()
+	async create() {
+		return this.productService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(HttpStatus.OK)
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: ProductDto) {
+		return this.productService.update(id, dto)
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.productService.delete(id)
+	}
 }
--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,4 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { generateSlug } from '../utils/generate-slug'
+import { PrismaService } from '../prisma.service'
+import { ProductDto } from './dto/product.dto'
+import { returnProductObject } from './returnProductObject.object'
+import { CategoryService } from '../category/category.service'
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+	constructor(
+		private prismaService: PrismaService,
+		private categoryService: CategoryService
+	) {}
+
+	async getAll(searchTerm: string): Promise<ProductDto[]> {
+		if (searchTerm) return this.search(searchTerm)
+
+		return this.prismaService.product.findMany({
+			select: returnProductObject,
+			orderBy: {
+				createdAt: 'desc'
+			}
+		})
+	}
+
+	async search(searchTerm: string): Promise<ProductDto[]> {
+		return this.prismaService.product.findMany({
+			where: {
+				OR: [
+					{
+						name: {
+							contains: searchTerm,
+							mode: 'insensitive'
+						}
+					},
+					{
+						description: {
+							contains: searchTerm,
+							mode: 'insensitive'
+						}
+					}
+				]
+			},
+			select: returnProductObject
+		})
+	}
+
+	async getBySlug(slug: string) {
+		const product = await this.prismaService.product.findUnique({
+			where: { slug },
+			select: returnProductObject
+		})
+
+		if (!product) throw new BadRequestException('Product not found')
+
+		return product
+	}
+
+	async getByCategory(categorySlug: string) {
+		const products = await this.prismaService.product.findMany({
+			where: {
+				category: {
+					slug: categorySlug
+				}
+			},
+			select: returnProductObject
+		})
+
+		if (!products) throw new BadRequestException('Products not found')
+
+		return products
+	}
+
+	async create() {
+		return this.prismaService.product.create({
+			data: {
+				name: '',
+				slug: '',
+				image: '',
+				description: '',
+				price: 0
+			}
+		})
+	}
+
+	async update(id: string, dto: ProductDto) {
+		const { description, price, image, name, categoryId } = dto
+
+		await this.categoryService.getById(categoryId)
+
+		return this.prismaService.product.update({
+			where: { id },
+			data: {
+				name,
+				description,
+				price,
+				slug: generateSlug(name),
+				category: {
+					connect: {
+						id: categoryId
+					}
+				}
+			}
+		})
+	}
+
+	async delete(id: string) {
+		return this.prismaService.product.delete({
+			where: { id }
+		})
+	}
+}
