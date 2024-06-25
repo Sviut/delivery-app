0586964e3d47566ccaad331bebd3b10939f67232
Sviut <jeleznov88@gmail.com>
2024-06-25 21:16:15 +0300

    add ui components

---
+++ mobile-app/app/components/ui/Loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+
+const Loader = () => {
+	return <ActivityIndicator size='large' color='#47AA52' />
+}
+
+export default Loader
