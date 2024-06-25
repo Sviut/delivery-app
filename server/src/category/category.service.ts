41c09336147c3275fe2799ab6401a2ddcd24ad2d
Sviut <jeleznov88@gmail.com>
2024-06-25 09:57:23 +0300

    implement category service

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { returnCategoryObject } from './return-category.object'
+import { CategoryDto } from './dto/category.dto'
+import { generateSlug } from '../utils/generate-slug'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private prismaService: PrismaService) {}
+
+	async getAll() {
+		return this.prismaService.category.findMany({
+			select: returnCategoryObject
+		})
+	}
+
+	async getById(id: string) {
+		const category = await this.prismaService.category.findUnique({
+			where: { id },
+			select: returnCategoryObject
+		})
+
+		if (!category) throw new BadRequestException('Category not found')
+
+		return category
+	}
+
+	async getBySlug(slug: string) {
+		const category = await this.prismaService.category.findUnique({
+			where: { slug },
+			select: returnCategoryObject
+		})
+
+		if (!category) throw new BadRequestException('Category not found')
+
+		return category
+	}
+
+	async create() {
+		return this.prismaService.category.create({
+			data: {
+				name: '',
+				slug: '',
+				image: ''
+			}
+		})
+	}
+
+	async update(id: string, dto: CategoryDto) {
+		return this.prismaService.category.update({
+			where: { id },
+			data: {
+				name: dto.name,
+				image: dto.image,
+				slug: generateSlug(dto.name)
+			}
+		})
+	}
+
+	async delete(id: string) {
+		return this.prismaService.category.delete({
+			where: { id }
+		})
+	}
+}
