a814559e1d637e80ea7d3dd0b3004d315c626b58
Sviut <jeleznov88@gmail.com>
2024-06-25 16:03:32 +0300

    update user module

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	Patch
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { Auth } from '../auth/auth/decorators/auth.decorator'
+import { CurrentUser } from '../auth/auth/decorators/user.decorator'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: string) {
+		return this.userService.getById(id)
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Auth()
+	@Patch('profile/favorites/:productId')
+	async toggleFavorite(
+		@CurrentUser('id') id: string,
+		@Param('productId') productId: string
+	) {
+		return this.userService.toggleFavorite(id, productId)
+	}
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { Prisma, User } from '@prisma/client'
+import { returnUserObject } from './return-user.object'
+import { returnProductObject } from '../product/returnProductObject.object'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private prismaService: PrismaService) {}
+
+	async getById(id: string, selectObject: Prisma.UserSelect = {}) {
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				id
+			},
+			select: {
+				...returnUserObject,
+				favorites: {
+					select: {
+						...returnProductObject
+					}
+				},
+				...selectObject
+			}
+		})
+
+		if (!user) throw new NotFoundException('User does not exist')
+
+		return user
+	}
+
+	async toggleFavorite(userId: string, productId: string) {
+		const user = await this.getById(userId)
+
+		if (!user) throw new UnauthorizedException('User does not exist')
+
+		const isExists = user.favorites.some(product => product.id === productId)
+
+		await this.prismaService.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				favorites: {
+					[isExists ? 'disconnect' : 'connect']: {
+						id: productId
+					}
+				}
+			}
+		})
+
+		return { message: 'Success' }
+	}
+}
