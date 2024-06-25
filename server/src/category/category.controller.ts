fda42251ec801d3e9a3cf955638d607757404ff0
Sviut <jeleznov88@gmail.com>
2024-06-25 10:36:57 +0300

    add categories routes

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,7 +1,54 @@
-import { Controller } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { CategoryService } from './category.service'
+import { Category } from '@prisma/client'
+import { CategoryDto } from './dto/category.dto'
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
+
+	@Get()
+	async getAll(): Promise<Category[]> {
+		return this.categoryService.getAll()
+	}
+
+	@Get('by-id/:id')
+	async getById(@Param('id') id: string): Promise<Category> {
+		return this.categoryService.getById(id)
+	}
+
+	@Get('by-slug/:slug')
+	async getBySlug(@Param('slug') slug: string): Promise<Category> {
+		return this.categoryService.getBySlug(slug)
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Post()
+	async create() {
+		return this.categoryService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(HttpStatus.OK)
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+		return this.categoryService.update(id, dto)
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Post()
+	async delete(@Param('id') id: string) {
+		return this.categoryService.delete(id)
+	}
 }
