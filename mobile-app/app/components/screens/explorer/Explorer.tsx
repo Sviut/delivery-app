37a7bae45b66ba538bc5cbe3572cba2fffc70fa9
Sviut <jeleznov88@gmail.com>
2024-06-26 15:53:51 +0300

    update routes

---
+++ mobile-app/app/components/screens/explorer/Explorer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Explorer = () => {
+	return <Text>Explorer</Text>
+}
+
+export default Explorer
--- mobile-app/app/navigation/routes.ts
+++ mobile-app/app/navigation/routes.ts
@@ -1,4 +1,8 @@
+import Explorer from '@/components/screens/explorer/Explorer'
+import Favorites from '@/components/screens/favorites/Favorites'
 import Home from '@/components/screens/home/Home'
+import Profile from '@/components/screens/profile/Profile'
+import Search from '@/components/screens/search/Search'
 
 import { IRoute } from '@/navigation/navigation.type'
 
@@ -6,5 +10,21 @@ export const routes: IRoute[] = [
 	{
 		name: 'Home',
 		component: Home
+	},
+	{
+		name: 'Favorites',
+		component: Favorites
+	},
+	{
+		name: 'Search',
+		component: Search
+	},
+	{
+		name: 'Explorer',
+		component: Explorer
+	},
+	{
+		name: 'Profile',
+		component: Profile
 	}
 ]
