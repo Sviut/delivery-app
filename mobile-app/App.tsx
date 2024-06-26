192280b782100b8cac2d44c61112123697b93532
Sviut <jeleznov88@gmail.com>
2024-06-26 15:11:31 +0300

    add auth provider & images

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
+import AuthProvider from '@/providers/auth/AuthProvider'
+
 import Navigation from '@/navigation/Navigation'
 
 export default function App() {
 	return (
-		<SafeAreaProvider>
-			<SafeAreaView style={{ flex: 1 }}>
-				<Navigation />
-			</SafeAreaView>
-		</SafeAreaProvider>
+		<AuthProvider>
+			<SafeAreaProvider>
+				<SafeAreaView style={{ flex: 1 }}>
+					<Navigation />
+				</SafeAreaView>
+			</SafeAreaProvider>
+		</AuthProvider>
 	)
 }
---
+++ mobile-app/app/providers/auth/AuthProvider.tsx
@@ -0,0 +1,47 @@
+import * as SplashScreen from 'expo-splash-screen'
+import React, {
+	FC,
+	PropsWithChildren,
+	createContext,
+	useEffect,
+	useState
+} from 'react'
+
+import {
+	IContext,
+	TypeUserState
+} from '@/providers/auth/auth-provider.interface'
+
+export const AuthContext = createContext({} as IContext)
+
+let ignore = SplashScreen.preventAutoHideAsync()
+
+const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+	const [user, setUser] = useState<TypeUserState>(null)
+
+	useEffect(() => {
+		let mounted = true
+
+		const checkAccessToken = async () => {
+			try {
+			} catch {
+			} finally {
+				await SplashScreen.hideAsync()
+			}
+		}
+
+		let ignore = checkAccessToken()
+
+		return () => {
+			mounted = false
+		}
+	}, [])
+
+	return (
+		<AuthContext.Provider value={{ user, setUser }}>
+			{children}
+		</AuthContext.Provider>
+	)
+}
+
+export default AuthProvider
